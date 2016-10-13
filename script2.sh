b095bb107afae0200b4c84df86c59aaa5be90a73
chasetoy <chase.a.toy@gmail.com>
2016-10-13 14:36:28 -0400

    adding script2.sh

---
+++ script2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#this script is a exercise to practice user input and output of said input
+#by the console
+
+echo "enter your first name"
+read first
+echo
+
+echo "enter your middle name"
+read middle
+echo
+
+echo "enter your last name"
+read last
+echo
+
+echo "your name is : $first $middle $last"
